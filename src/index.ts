cc3bfe7e8d5fd5758f5b46e3686143b74cd69381
Ziad Tarek <ziadtarekfa@gmail.com>
2023-01-19 01:13:21 +0200

    feat:resize image and save it in cache

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import fs from "fs";
 import Path from "path";
+import sharp from "sharp";
 const app = express();
 
-
+let path: string;
 app.get('/images', (req: express.Request, res: express.Response) => {
     const width = (req.query.width as unknown) as number;
     const height = (req.query.height as unknown) as number;
@@ -17,16 +18,42 @@ app.get('/images', (req: express.Request, res: express.Response) => {
     }
     else if (!checkIfFileNameExists(fileName)) {
         res.send("Image does not exist");
+
+    }
+    // check if file exists in cache
+    const thumbPath = Path.join(__dirname, `C:/Users/dell/OneDrive/Desktop/Image Processing API/images/thumbs/${fileName}_${width}_${height}.jpg`);
+    console.log(thumbPath);
+    const fileNameExists: boolean = fs.existsSync(thumbPath);
+
+    console.log(thumbPath);
+    if (fileNameExists) {
+        res.sendFile(thumbPath);
+    }
+    else {
+        // Resize file and save it to cache
+
+        sharp(path).resize(320, 200).toFile(`./images/thumbs/${fileName}_${width}_${height}.jpg`, () => {
+            console.log("save is successful");
+        });
+
+
     }
 
 
 });
 
-const checkIfFileNameExists = (fileName: string): boolean => {
-    const path = Path.join(__dirname, `../images/${fileName}.jpg`);
-    const fileNameExists: boolean = fs.existsSync(path);
 
+
+
+const checkIfFileNameExists = (fileName: string): boolean => {
+    path = Path.join(__dirname, `../images/${fileName}.jpg`);
+    const fileNameExists: boolean = fs.existsSync(path);
     return fileNameExists;
+}
+async function saveFileToCache(fileName: string, width: number, height: number) {
+    await sharp(path).resize(320, 200).toFormat('jpg').toFile(`../images/thumbs/${fileName}_${width}_${height}`, () => {
+        console.log("save is successful");
+    });
 }
 
 app.listen(3000, () => {
